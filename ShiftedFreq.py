08f4d9a008ce6d17cd6b0f073f27c175f2a0c812
Dylan Tisdall <mtisdall@pennmedicine.upenn.edu>
2024-10-23 14:48:07 +0000

    Now running on radios

--- ShiftedFreq.py
+++ ShiftedFreq.py
@@ -12,7 +12,6 @@ from packaging.version import Version as StrictVersion
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio import analog
-from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -22,6 +21,8 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import uhd
+import time
 from xmlrpc.server import SimpleXMLRPCServer
 import threading
 import sip
@@ -64,7 +65,7 @@ class ShiftedFreq(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 1024000
         self.rmt_delta_freq = rmt_delta_freq = 0
 
         ##################################################
@@ -76,6 +77,36 @@ class ShiftedFreq(gr.top_block, Qt.QWidget):
         self.xmlrpc_server_0_thread = threading.Thread(target=self.xmlrpc_server_0.serve_forever)
         self.xmlrpc_server_0_thread.daemon = True
         self.xmlrpc_server_0_thread.start()
+        self.uhd_usrp_sink_0_0 = uhd.usrp_sink(
+            ",".join(("serial=32CAB89", '', "master_clock_rate=30.72e6")),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='peak=0.003906',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0_0.set_samp_rate(1000000)
+        # No synchronization enforced.
+
+        self.uhd_usrp_sink_0_0.set_center_freq(2523200000, 0)
+        self.uhd_usrp_sink_0_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0_0.set_gain(80, 0)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("serial=32CAB7A", '', "master_clock_rate=30.72e6")),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='peak=0.003906',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(1000000)
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_sink_0.set_center_freq(2523200000, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0.set_gain(80, 0)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
@@ -169,20 +200,16 @@ class ShiftedFreq(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, rmt_delta_freq, 1, 0, 0)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
+        self.analog_const_source_x_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, 1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle2_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.uhd_usrp_sink_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -198,9 +225,6 @@ class ShiftedFreq(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(1000, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
@@ -209,7 +233,6 @@ class ShiftedFreq(gr.top_block, Qt.QWidget):
 
     def set_rmt_delta_freq(self, rmt_delta_freq):
         self.rmt_delta_freq = rmt_delta_freq
-        self.analog_sig_source_x_1.set_frequency(self.rmt_delta_freq)
 
 
 
